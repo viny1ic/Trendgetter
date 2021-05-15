b9891af60fc84b7e5fba0b811d6fff3f618e23ef
vinayak dubey <vinayak.dubey2019@vitstudent.ac.in>
2021-05-15 20:21:06 +0530

    reddit.js

--- reddit.js
+++ reddit.js
@@ -1,6 +1,7 @@
 var firebase = require('firebase');
 var request = require('request')
 var dotenv = require('dotenv').config()
+var fs = require('fs')
 
 var firebaseConfig = {
     apiKey: process.env.apiKey,
@@ -19,12 +20,17 @@ var to_set = {
     jewelry: null
 }
 
-setData('https://www.reddit.com/r/Watches.json')
-setData('https://www.reddit.com/r/jewelry.json')
-setData('https://www.reddit.com/r/mensfashion.json')
-setData('https://www.reddit.com/r/femalefashion.json')
-setData('https://www.reddit.com/r/Sneakers.json')
-
+// setData('https://www.reddit.com/r/Watches.json')
+// setData('https://www.reddit.com/r/jewelry.json')
+// setData('https://www.reddit.com/r/mensfashion.json')
+// setData('https://www.reddit.com/r/femalefashion.json')
+// setData('https://www.reddit.com/r/Sneakers.json')
+// setData('https://www.reddit.com/r/ItalianFood.json')
+// setData('https://www.reddit.com/r/chinesefood.json')
+// setData('https://www.reddit.com/r/mexicanfood.json')
+// setData('https://www.reddit.com/r/JapaneseFood.json')
+// setData('https://www.reddit.com/r/frenchfood.json')
+setData('https://www.reddit.com/r/movies.json')
 function setData(url){
   request(url, { json: true }, (err, res, body) => {
     if (err) { return console.log(err); }
@@ -34,21 +40,36 @@ function setData(url){
     });
 }
 
-
+var name = 0
 function getData(data){
     var ans = {}
     for(x in data){
         if(data[x].data.is_video ==false && data[x].data.thumbnail!='self'){
           var img = data[x].data.url_overridden_by_dest
           try{
-              if(img.slice(img.length - 4)==".jpg" || img.slice(img.length - 4)==".png" || data[x].data.title != undefined)
+              if(img.slice(img.length - 4)==".jpg" || data[x].data.title != undefined)
                 var title = data[x].data.title
+                var link = "https://reddit.com"+ data[x].data.permalink
                 ans[data[x].data.name] = {
-                    img, title
+                    img, title, link
                 }
+                try{
+                download(img, "C:/Users/Asus/Projects/Trendgetter/img/movie/"+name+".jpg", function(){console.log(img + " downloaded")})
+                }
+                finally{continue}
               }
           finally{continue}
           }
         }
         return ans
     }
+
+    var download = function(uri, filename, callback){
+      name+= 1
+      request.head(uri, function(err, res, body){
+      console.log('content-type:', res.headers['content-type']);
+      console.log('content-length:', res.headers['content-length']);
+      
+      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+      });
+      };
